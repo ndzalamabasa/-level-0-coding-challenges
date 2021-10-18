fb7597e87e301fd6e1395823cfe0d88def6b4981
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2021-10-18 09:30:14 +0200

    Added file 0.10

---
+++ task-0.10.js
@@ -0,0 +1,21 @@
+function getCommonLetters(firstString, secondString) {
+  firstString = firstString.toLowerCase();
+  secondString = secondString.toLowerCase();
+
+  let commonLettersArray = [];
+
+  for (let i = 0; i < firstString.length; i++) {
+    for (let j = 0; j < secondString.length; j++) {
+      if (firstString[i] === secondString[j]) {
+        if (!(commonLettersArray.indexOf(secondString[j]) !== -1)) {
+          commonLettersArray.push(secondString[j]);
+        }
+      }
+    }
+  }
+
+  let commonLettersString = commonLettersArray.toString();
+  console.log(`Common letters: ${commonLettersString}`);
+}
+
+getCommonLetters('Mheho Bravery', 'Amukelani');
