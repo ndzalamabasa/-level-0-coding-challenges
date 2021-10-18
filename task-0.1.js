0a6eb4a93444b7008902b6f47d29ea116faf22ab
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2021-10-18 09:18:44 +0200

    Added task 0.1

---
+++ task-0.1.js
@@ -0,0 +1,9 @@
+let x = 0;
+let y = 1;
+console.log(`The value of x is: ${x}`);
+console.log(`The value of y is: ${y}`);
+
+x += 3;
+y += x;
+console.log(`The value of x after adding 3 is: ${x}`);
+console.log(`The value of y after adding x to y is: ${y}`);
